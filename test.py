04b94569f539a5a9482d4a58fd144ded85b92cdc
Nadia Zlobina <nadia3d@Nadias-Air.fritz.box>
2023-12-25 21:44:50 +0100

    create sicp

---
+++ test.py
@@ -0,0 +1,73 @@
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+class LinkedList:
+  def __init__(self, value=None):
+    if value is not None:
+        self.head_node = Node(value)
+    else:
+        self.head_node = None
+  
+  def get_head_node(self):
+    return self.head_node
+  
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+    
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+  
+  def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+      self.head_node = current_node.get_next_node()
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None
+        else:
+          current_node = next_node
+
+
+def find_middle(linked_list):
+  finder = linked_list.head_node
+  middle = linked_list.head_node
+  while finder:
+    finder = finder.get_next_node()
+    if finder:
+      finder = finder.get_next_node()
+      middle = middle.get_next_node()
+  return middle
+
+def generate_test_linked_list(length):
+  linked_list = LinkedList()
+  for i in range(length,0,-1):
+    linked_list.insert_beginning(i)
+  return linked_list
+
+# Use this to test your code:
+test_list = generate_test_linked_list(7)
+print(test_list.stringify_list())
+middle_node = find_middle(test_list)
+print(middle_node.get_value())
